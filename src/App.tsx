c15495db89439d03c4e90a71d3da5a1f893178a4
jeginilokeshreddy <jeginilokesh@gmail.com>
2025-09-20 14:17:07 +0530

    Collect all from filed vlaues with the onSubmit new FormData(data1.target);Object.fromEntries(formData);

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <form onSubmit={(formdata)=>{
-        console.log(formdata,"formdata")
+
+      <form onSubmit={(data1)=>{
+        data1?.preventDefault()
+        const formData = new FormData(data1.target); // event.target is the <form>
+  const data = Object.fromEntries(formData);  // convert FormData to object
+  console.log(data); // { username: "value", password: "value" }
+       
+        //  const data = Object.fromEntries(formData); // convert to object
+    // console.log(data); // { username: "value", password: "value" }
+        console.log(formData,"formdata")
+        
       }}>
         <input type="text" name="username" placeholder='username'/>
         <input type="password" name="password" placeholder='password'/>
